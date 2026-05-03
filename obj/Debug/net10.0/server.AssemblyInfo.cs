9ffe8f12526bf87829e1b23e0a0b412d59a5118a
Atif Mostoba <atifmostoba456@gmail.com>
2026-05-04 04:07:50 +0600

    all new api updated

--- obj/Debug/net10.0/server.AssemblyInfo.cs
+++ obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+661490806167a77cf1283dd203827920fb44d879")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29f263cc1429d4d49e2aaf34b288487d8c131085")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
